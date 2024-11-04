378996c746916837e84712093f16db18325490a6
qavelikii <qavelikii@gmail.com>
2024-11-04 12:26:25 -0500

    Описание изменений

--- cypress/e2e/comptia_home_page.cy.js
+++ cypress/e2e/comptia_home_page.cy.js
@@ -1,25 +1,38 @@
 /// <reference types="cypress" />
 
-describe('Проверка главной страницы CompTIA', () => {
+describe('CompTIA Homepage Test', () => {
   beforeEach(() => {
+    cy.on('uncaught:exception', (err, runnable) => {
+      // возвращаем false, чтобы Cypress не прерывал тесты
+      return false;
+    });
+    cy.viewport(1280, 720);
     cy.visit('https://www.comptia.org');
   });
 
-  it('Должен отображать заголовок страницы', () => {
+  it('Should display the correct page title', () => {
     cy.title().should('include', 'CompTIA');
   });
 
-  it('Кнопка "Get Started" должна быть кликабельна', () => {
+  it('Get Started button should be clickable', () => {
     cy.contains('Get Started').should('be.visible').click();
     cy.url().should('include', '/certifications');
   });
 
-  it('Навигационное меню должно содержать все основные разделы', () => {
-    cy.get('nav').within(() => {
-      cy.contains('Certifications').should('be.visible');
-      cy.contains('Training').should('be.visible');
-      cy.contains('Resources').should('be.visible');
-      cy.contains('About').should('be.visible');
+  it('Navigation menu should contain all main sections', () => {
+    // Нажимаем на иконку меню, если она существует
+    cy.get('button.menu-toggle').then(($menuButton) => {
+      if ($menuButton.is(':visible')) {
+        cy.wrap($menuButton).click();
+      }
+    });
+
+    // Убедиться, что навигационное меню видно и содержит все основные разделы
+    cy.get('nav.navbar_menu.w-nav-menu', { timeout: 20000 }).should('be.visible').within(() => {
+      cy.contains('Certifications', { timeout: 20000 }).should('be.visible');
+      cy.contains('Training', { timeout: 20000 }).should('be.visible');
+      cy.contains('Resources', { timeout: 20000 }).should('be.visible');
+      cy.contains('Career Changers', { timeout: 20000 }).should('be.visible');
     });
   });
 });
